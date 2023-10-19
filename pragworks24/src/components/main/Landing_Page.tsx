67cbdbe3f1096db8bbd5b07887cfefd9342496d3
JOAO TARRAN <joaotarran@gmail.com>
2023-10-19 11:50:16 -0300

    Contato, Landing Page Updates e Section Dividers

--- pragworks24/src/components/main/Landing_Page.tsx
+++ pragworks24/src/components/main/Landing_Page.tsx
@@ -20,17 +20,29 @@ const LandingPage = forwardRef(function LandingPage(props, ref: any) {
     return (
         <>
             <Section_Inicio />
-            <div className="Section_Divider" id="SD01" key={"SD01_key"}></div>
+            <div className="Section_Divider Section_Divider_With_Text" id="SD01" key={"SD01_key"}>
+                <h1 className="Section_Divider_Text">Produtos</h1>
+            </div>
             <Section_Produtos />
-            <div className="Section_Divider" id="SD02" key={"SD02_key"}></div>
+            <div className="Section_Divider Section_Divider_With_Text" id="SD02" key={"SD02_key"}>
+                <h1 className="Section_Divider_Text">Soluções</h1>
+            </div>
             <Section_Solucoes />
-            <div className="Section_Divider" id="SD03" key={"SD03_key"}></div>
+            <div className="Section_Divider Section_Divider_With_Text" id="SD03" key={"SD03_key"}>
+                <h1 className="Section_Divider_Text">Sobre Nós</h1>
+            </div>
             <Section_Sobre />
-            <div className="Section_Divider" id="SD04" key={"SD04_key"}></div>
+            <div className="Section_Divider Section_Divider_With_Text" id="SD04" key={"SD04_key"}>
+                <h1 className="Section_Divider_Text">Contato</h1>
+            </div>
             <Section_Contato />
-            <div className="Section_Divider" id="SD05" key={"SD05_key"}></div>
+            <div className="Section_Divider Section_Divider_With_Text" id="SD05" key={"SD05_key"}>
+                <h1 className="Section_Divider_Text">Menu</h1>
+            </div>
             <Section_Menu />
-            <div className="Section_Divider" id="SD06" key={"SD06_key"}></div>
+            <div className="Section_Divider Section_Divider_With_Text" id="SD06" key={"SD06_key"}>
+                <h1 className="Section_Divider_Text">Human Robotics</h1>
+            </div>
         </>
     );
 });
--- pragworks24/src/components/sections/Contato.tsx
+++ pragworks24/src/components/sections/Contato.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState, forwardRef } from "react";
 
 //Framer Motion Imports
-import { motion as m, AnimatePresence } from "framer-motion";
+import { motion as m, AnimatePresence, LayoutGroup } from "framer-motion";
 
 //React Redux Imports
 import { useDispatch } from "react-redux";
@@ -12,6 +12,17 @@ const Section_Contato = forwardRef(function Section_Contato(props, ref: any) {
     const dispatch = useDispatch();
     const setActiveSectionFunction = (id: number) => {
         dispatch(setActiveSection(id));
+    };
+
+    const [map1isOpen, setMap1isOpen] = useState(true);
+    const [map2isOpen, setMap2isOpen] = useState(false);
+
+    const toggleMap1 = () => {
+        setMap1isOpen(!map1isOpen);
+    };
+
+    const toggleMap2 = () => {
+        setMap2isOpen(!map2isOpen);
     };
 
     return (
@@ -29,20 +40,118 @@ const Section_Contato = forwardRef(function Section_Contato(props, ref: any) {
         >
             <h1 className="LP_Section_Title">Contato</h1>
             <div className="Contact_Container">
-                <h2>Entre em Contato, estamos a disposição!</h2>
-                <button>Envie um email</button>
-                <button>Entre em Contato pelo Whatsapp</button>
-                <button>Inscreva-se na Newsletter</button>
-                <div className="Contact_Form_Container">
-                    <form className="Contact_Form">
-                        <input type="text" placeholder="Nome" />
-                        <input type="email" placeholder="Email" />
-                        <input type="text" placeholder="Assunto" />
-                        <textarea placeholder="Mensagem" />
-                        <button type="submit">Enviar</button>
-                    </form>
+                <h3 className="Contact_CallToAction">Sinta-se a vontade para entrar em contato, estamos de prontidão para lhe atender!</h3>
+                <div className="Contact_Info">
+                    <div className="Contact_Info_Item">
+                        <h3>Telefone:</h3>
+                        <p className="Contact_Info_Item_Detail">+55 (41) 999 999 999</p>
+                    </div>
+
+                    <div className="Contact_Info_Item">
+                        <h3 className="Contact_Info_Title">Email:</h3> <p className="Contact_Info_Item_Detail">contato@humanrobotics.com.br</p>
+                    </div>
+
+                    <div className="Contact_Info_Item">
+                        <h3 className="Contact_Info_Title">
+                            Escritório
+                            <button className="Contact_Location_Btn hoverable" onClick={toggleMap1}>
+                                <span className="material-icons">map</span>Ver Mapa
+                            </button>
+                        </h3>
+                        <p className="Contact_Info_Item_Detail">Curitiba, Paraná, Brasil.</p>
+                    </div>
+
+                    <div className="Contact_Info_Item">
+                        <h3 className="Contact_Info_Title">
+                            Laboratório
+                            <button className="Contact_Location_Btn hoverable" onClick={toggleMap2}>
+                                <span className="material-icons">map</span>Ver Mapa
+                            </button>
+                        </h3>
+                        <p className="Contact_Info_Item_Detail">Curitiba, Paraná, Brasil. </p>
+                    </div>
+                </div>
+                <div className="Contact_Action_Container">
+                    <a className="Contact_Action_Btn Btn_Class hoverable" href="mailto:recipient@example.com">
+                        Enviar Email
+                    </a>
+                    <a
+                        className="Contact_Action_Btn Btn_Class hoverable"
+                        href="https://api.whatsapp.com/send?phone=5541999977955&text=Ol%C3%A1"
+                        target="_blank"
+                    >
+                        Enviar Mensagem por WhatsApp
+                    </a>
+                    <a
+                        className="Contact_Action_Btn Btn_Class hoverable"
+                        href="https://api.whatsapp.com/send?phone=5541999977955&text=Ol%C3%A1"
+                        target="_blank"
+                    >
+                        Inscreva-se na Newsletter
+                    </a>
                 </div>
             </div>
+            <m.div layout layoutRoot className="Maps_Master_Container">
+                {map1isOpen && (
+                    <m.div
+                        layout="preserve-aspect"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className="Map_Container Map_1"
+                        key="mapa_01"
+                    >
+                        <div className="Map_Header">
+                            <span className="material-icons Info_Icon">info</span>
+                            <h3 className="Map_Title">Mapa para o Escritório</h3>
+                            <button className="Map_Close_Btn hoverable" onClick={toggleMap1}>
+                                <span className="material-icons">close</span>
+                            </button>
+                        </div>
+                        <div className="Map_Body">
+                            <iframe
+                                className="Map_Iframe"
+                                title="Google Maps"
+                                width="100%"
+                                height="100%"
+                                style={{ border: 0 }}
+                                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDyXfuRSunXyy-YO9HSag0AFrMJexBPcZA&q=Curitiba`}
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                    </m.div>
+                )}
+
+                {map2isOpen && (
+                    <m.div
+                        layout="preserve-aspect"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className="Map_Container Map_2"
+                        key="mapa_02"
+                    >
+                        <div className="Map_Header">
+                            <span className="material-icons">info</span>
+                            <h3 className="Map_Title">Mapa para o Laboratório</h3>
+                            <button className="Map_Close_Btn hoverable" onClick={toggleMap2}>
+                                <span className="material-icons">close</span>
+                            </button>
+                        </div>
+                        <div className="Map_Body">
+                            <iframe
+                                className="Map_Iframe"
+                                title="Google Maps"
+                                width="100%"
+                                height="100%"
+                                style={{ border: 0 }}
+                                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDyXfuRSunXyy-YO9HSag0AFrMJexBPcZA&q=Curitiba`}
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                    </m.div>
+                )}
+            </m.div>
         </m.div>
     );
 });
